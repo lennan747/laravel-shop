78d96d3ca9266b80ffd4b9d2a6814e46cfda824a
lennan747 <lennan747@gmail.com>
2019-09-03 11:44:06 +0800

    用户界面 - 订单详情

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -70,4 +70,14 @@ class OrdersController extends Controller
         $this->dispatch(new CloseOrder($order, config('app.order_ttl')));
         return $order;
     }
+
+    public function index(Request $request)
+    {
+        $orders = Order::query()
+            ->with(['items.product', 'items.productSku'])
+            ->where('user_id', $request->user()->id)
+            ->orderBy('created_at', 'desc')
+            ->paginate();
+        return view('orders.index', ['orders' => $orders]);
+    }
 }
