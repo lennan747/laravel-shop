2f9a6b51ea522f57d8bb39fc7be1069109de558a
lennan747 <lennan747@gmail.com>
2019-09-03 15:45:12 +0800

    订单支付

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,6 +18,8 @@ class AppServiceProvider extends ServiceProvider
         //
         $this->app->singleton('alipay',function(){
             $config = config('pay.alipay');
+            $config['notify_url'] = 'http://requestbin.net/r/rr2fhlrr';//route('payment.alipay.notify');
+            $config['return_url'] = route('payment.alipay.return');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
                 $config['mode']         = 'dev';
