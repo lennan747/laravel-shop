fcb19752b39e50d62cc576e7890b117033cc4426
lennan747 <lennan747@gmail.com>
2019-09-04 15:56:54 +0800

    后台管理 - 同意退款 - 支付宝退款

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,7 @@ class AppServiceProvider extends ServiceProvider
         //
         $this->app->singleton('alipay',function(){
             $config = config('pay.alipay');
-            $config['notify_url'] = 'http://requestbin.net/r/z5b5d1z5';//route('payment.alipay.notify');
+            $config['notify_url'] = 'http://requestbin.net/r/wsvlzyws';//route('payment.alipay.notify');
             $config['return_url'] = route('payment.alipay.return');
             // 判断当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
